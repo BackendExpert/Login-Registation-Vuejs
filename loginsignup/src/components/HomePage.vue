1420f1f396c141d1dc6822f798ca37273939f95a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-25 13:38:02 +0530

    Update HomePage.vue
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- loginsignup/src/components/HomePage.vue
+++ loginsignup/src/components/HomePage.vue
@@ -2,8 +2,14 @@
     <div class="bg-cover bg-center h-screen min-h-screen" :style="{ backgroundImage: `linear-gradient(to right, #001B3A, rgba(0,0,0,0.4)), url('https://wallpapercave.com/wp/wp2707503.jpg')` }">
         <div class="" id="home">
             <AppHeader />
-            <div class="text-center my-16" >
-                <h1 class="text-white lg:text-5xl site-title">Buy Your Dream car</h1>
+            <div class="text-center my-16 text-white mx-16" >
+                <h1 class="lg:text-5xl site-title">Buy Your Dream car</h1>
+                <p class="py-8">with JKCARs</p>
+
+                <p class="my-4">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. In quidem, autem facere molestias modi dolorum iste. Hic nam vero obcaecati distinctio exercitationem suscipit, illum nobis porro, quis fuga fugiat numquam!
+                    Illo commodi fugit ullam officiis at id sit cumque inventore quo est facere dolores consequuntur ad distinctio aut esse doloremque, assumenda eaque tenetur qui provident dolorum rerum beatae a? Iste.
+                </p>
             </div>
         </div>
     </div>
