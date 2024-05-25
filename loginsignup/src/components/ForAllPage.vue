0c58968261592c1c0ec7c34989cb72191849fc2c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-25 12:59:10 +0530

    Update ForAllPage.vue
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- loginsignup/src/components/ForAllPage.vue
+++ loginsignup/src/components/ForAllPage.vue
@@ -1,3 +1,11 @@
 <script>
+  import { BuildingOffice2Icon } from '@heroicons/vue/24/solid';
+
+  export default {
+    name: "ForAllPages",
+    components: {
+        BuildingOffice2Icon
+    },
+  }
 
 </script>
