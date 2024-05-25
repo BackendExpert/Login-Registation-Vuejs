1d6213ff33a7461f5a76bbfcebddc383b39b9986
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-25 13:31:59 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- loginsignup/src/components/HomePage.vue
+++ loginsignup/src/components/HomePage.vue
@@ -2,19 +2,16 @@
     <div class="bg-cover bg-center h-screen min-h-screen" :style="{ backgroundImage: `linear-gradient(to right, #001B3A, rgba(0,0,0,0.4)), url('https://wallpapercave.com/wp/wp2707503.jpg')` }">
         <div class="">
             <AppHeader />
-            <AppNav />
         </div>
     </div>
 </template>
 
 <script>
     import AppHeader from './AppHeader.vue';
-    import AppNav from './AppNav.vue'
     export default {
         name: "HomePage",
         components: {
             AppHeader,
-            AppNav
         }
     }
 </script>
