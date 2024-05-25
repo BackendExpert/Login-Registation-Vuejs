37249688a8ad341ce068aca238a2b652ab98813c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-25 15:09:43 +0530

    Update AppSignUp.vue
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- loginsignup/src/components/AppSignUp.vue
+++ loginsignup/src/components/AppSignUp.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="">
         <div class="">
-            <form>
+            <form @submit.prevent="headleSignUp">
                 <div class="">
                     <label for="" class="text-xl block my-2">Username : </label>
-                    <input type="text" name="" id="" class="h-12 2-full bg-white text-gray-700 rounded pl-2" required placeholder="Enter Username">
+                    <input type="text" name="" id="" v-model="signUpData.username" class="h-12 2-full bg-white text-gray-700 rounded pl-2" required placeholder="Enter Username">
                 </div>
                 <div class="">
                     <label for="" class="text-xl block my-2">Email : </label>
-                    <input type="email" name="" id="" class="h-12 2-full bg-white text-gray-700 rounded pl-2" required placeholder="Enter Email Address">
+                    <input type="email" name="" id="" v-model="signUpData.email" class="h-12 2-full bg-white text-gray-700 rounded pl-2" required placeholder="Enter Email Address">
                 </div>
                 <div class="">
                     <label for="" class="text-xl block my-2">Password : </label>
-                    <input type="password" name="" id="" class="h-12 2-full bg-white text-gray-700 rounded pl-2" required placeholder="Enter Password">
+                    <input type="password" name="" id="" v-model="signUpData.password" class="h-12 2-full bg-white text-gray-700 rounded pl-2" required placeholder="Enter Password">
                 </div>
                 <div class="">
                     <button type="submit" class="bg-green-500 my-4 py-2 px-8 rounded text-white">SignUp</button>
@@ -23,7 +23,35 @@
 </template>
 
 <script>
+    import axios from 'axios';
+
     export default {
-        name: "AppSignUp"
+        name: "AppSignUp",
+
+        data() {
+            return {
+                signUpData: {
+                    username: '',
+                    email: '',
+                    password: ''
+                }           
+            }
+        },
+
+        methods: {
+            headleSignUp() {
+                axios.post('http://localhost:8081/SignUp', this.signUpData)
+                .then(res => {
+                    if(res.data.Status === "Success") {
+                        alert("Registation Successful")
+                    }
+                    else{
+                        alert(res.data.Error)
+                    }
+                })
+            }
+        }
     }
+    
+    
 </script>
