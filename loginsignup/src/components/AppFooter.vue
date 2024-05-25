6ade929ff592f77bad5d864bc5e6eab8ce343636
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-25 14:21:57 +0530

    Update AppFooter.vue
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- loginsignup/src/components/AppFooter.vue
+++ loginsignup/src/components/AppFooter.vue
@@ -1,16 +1,29 @@
 <template>
-    <div class="px-16">
+    <div class="px-16 py-16 bg-gray-800">
         <div class="md:grid grid-cols-3 gap-4 my-4">
-            <div class="lg:text-3xl text-xl text-gray-500 font-semibold">
-                <h1 class="">JKCars</h1>
+            <div class="text-white font-semibold">
+                <h1 class="lg:text-3xl text-xl text-center">JKCars</h1>
+                <p class="text-xl my-4">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro repellendus nulla alias beatae cupiditate minus amet impedit, magni provident quasi qui explicabo a consectetur vero cumque laborum rem dolorum delectus!
+                </p>
             </div>
-            <div class="lg:text-3xl text-xl text-gray-500 font-semibold">
-                <h1 class="">Links</h1>
+            <div class="lg:text-3xl text-xl text-white font-semibold text-center">
+                <h1 class="lg:text-3xl text-xl text-center">Links</h1>
+                <p class="text-xl my-4">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro repellendus nulla alias beatae cupiditate minus amet impedit, magni provident quasi qui explicabo a consectetur vero cumque laborum rem dolorum delectus!
+                </p>
             </div>
-            <div class="lg:text-3xl text-xl text-gray-500 font-semibold">
-                <h1 class="">Contact Info</h1>
+            <div class="lg:text-3xl text-xl text-white font-semibold text-center">
+                <h1 class="lg:text-3xl text-xl text-center">Contact Infor</h1>
+                <p class="text-xl my-4">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro repellendus nulla alias beatae cupiditate minus amet impedit, magni provident quasi qui explicabo a consectetur vero cumque laborum rem dolorum delectus!
+                </p>
             </div>
         </div>
+        <hr class="py-4">     
+        <p class="text-center text-white">
+            &copy; JKCARS developed using Vue.js
+        </p>   
     </div>
 </template>
 
