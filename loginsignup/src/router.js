eaaec68ec24d864abbcc968e3bf6098418f13b7b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-25 15:55:19 +0530

    Update router.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- loginsignup/src/router.js
+++ loginsignup/src/router.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomePage from './components/HomePage.vue'
+import HomePage from './components/HomePage.vue';
+import DashBoard from './components/DashBoard.vue';
 
 
 const routes = [
   { path: '/', component: HomePage },
+  { path: '/DashBoard', component: DashBoard },
 ];
 
 const router = createRouter({
