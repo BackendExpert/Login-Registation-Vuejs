9f6ceaef960be10b1a4a628e1c431f7ba0d78b78
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-25 13:55:21 +0530

    Update LatestModels.vue
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- loginsignup/src/components/LatestModels.vue
+++ loginsignup/src/components/LatestModels.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="bg-white py-16 lg:px-20 md:px-12">
-        Latest Models
+    <div class="bg-white py-16 lg:px-20 md:px-12">        
+        <h1 class="py-6 text-center lg:text-4xl md:text-2xl text-gray-500">Latest Models</h1>
     </div>
 </template>
 
