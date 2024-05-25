4deb491f06ea7210ea1d9058fea830b52ec862f9
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-25 13:33:29 +0530

    Update HomePage.vue
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- loginsignup/src/components/HomePage.vue
+++ loginsignup/src/components/HomePage.vue
@@ -2,6 +2,9 @@
     <div class="bg-cover bg-center h-screen min-h-screen" :style="{ backgroundImage: `linear-gradient(to right, #001B3A, rgba(0,0,0,0.4)), url('https://wallpapercave.com/wp/wp2707503.jpg')` }">
         <div class="">
             <AppHeader />
+            <div class="text-center my-16">
+                <h1 class="text-white lg:text-5xl">Buy Your Dream car</h1>
+            </div>
         </div>
     </div>
 </template>
