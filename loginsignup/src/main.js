3887c79b383fca1fb461189556572d04b896b1f9
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-25 15:29:13 +0530

    Update main.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- loginsignup/src/main.js
+++ loginsignup/src/main.js
@@ -2,12 +2,12 @@ import App from './App.vue'
 import './assets/tailwind.css'
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-
+import HomePage from './components/HomePage.vue'
 
 Vue.use(VueRouter);
 
 const routes = [
-
+    { path: '/', component: HomePage },
 ];
 
 const router = new VueRouter({
