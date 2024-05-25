a113e41adb3670acdbf3fbaf93aa0b332d64099a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-25 14:14:32 +0530

    Create AppFooter.vue
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ loginsignup/src/components/AppFooter.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="">
+        <div class="md:grid gird-cols-3 gap-4">
+            <div class="lg:text-3xl text-xl text-gray-500 font-semibold">
+                <h1 class="">JKCars</h1>
+            </div>
+            <div class="lg:text-3xl text-xl text-gray-500 font-semibold">
+                <h1 class="">Links</h1>
+            </div>
+            <div class="lg:text-3xl text-xl text-gray-500 font-semibold">
+                <h1 class="">Contact Info</h1>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "AppFooter"
+    }
+</script>
