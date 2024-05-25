e3b9ba799622b63b92b23c8c68329375b81137eb
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-25 14:38:25 +0530

    Update AppLogin.vue
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- loginsignup/src/components/AppLogin.vue
+++ loginsignup/src/components/AppLogin.vue
@@ -1,6 +1,17 @@
 <template>
     <div class="">
-        Login To System
+        <div class="">
+            <form>
+                <div class="">
+                    <label for="" class="text-xl block my-2">Email : </label>
+                    <input type="email" name="" id="" class="h-12 2-full bg-white text-gray-700 rounded pl-2" required placeholder="Enter Email Address">
+                </div>
+                <div class="">
+                    <label for="" class="text-xl block my-2">Password : </label>
+                    <input type="password" name="" id="" class="h-12 2-full bg-white text-gray-700 rounded pl-2" required placeholder="Enter Password">
+                </div>
+            </form>
+        </div>
     </div>
 </template>
 
