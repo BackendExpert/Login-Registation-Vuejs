5b4f316ac4c7e9aa6b0011b9fa913a1b98a6ea71
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-25 15:44:21 +0530

    Update main.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- loginsignup/src/main.js
+++ loginsignup/src/main.js
@@ -1,28 +1,6 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router';
-import App from './App.vue';
-
-
-
-import { createApp } from 'vue';
+import { createApp } from 'vue'
+import App from './App.vue'
 import './assets/tailwind.css'
 
-
-import HomePage from './components/HomePage.vue'
-
-Vue.use(VueRouter);
-
-const routes = [
-    { path: '/', component: HomePage },
-];
-
-const router = new VueRouter({
-  routes
-});
-
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app');
 
 createApp(App).mount('#app')
