e92a490b550a7ed9f7846a7f9b01a400bb47c21c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-25 13:53:43 +0530

    Update AboutAs.vue
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- loginsignup/src/components/AboutAs.vue
+++ loginsignup/src/components/AboutAs.vue
@@ -1,6 +1,13 @@
 <template>
-    <div class="bg-gray-200 my-8">
-        <h1 class="py-6 text-center lg:text-4xl md:text-2xl">About Us</h1>
+    <div class="bg-gray-200 py-16 lg:px-20 md:px-12">
+        <h1 class="py-6 text-center lg:text-4xl md:text-2xl text-gray-500">About Us</h1>
+        <p class="">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum eius sed itaque autem facilis ipsam alias debitis doloremque unde vel. Quis animi officia sequi totam praesentium perferendis pariatur ipsum ducimus?
+            Eum nam laborum, culpa tempora soluta, doloribus distinctio, sint nobis eaque excepturi minima incidunt alias? Facilis aut aliquid fuga quos aspernatur magnam unde distinctio possimus quaerat doloribus optio, consequuntur deleniti.
+            Animi libero magni architecto nostrum, quam autem sed quos voluptate. Incidunt saepe sapiente est officia architecto? Sunt, voluptate? Velit ea, inventore voluptates esse saepe labore sed iusto delectus ipsam aliquid.
+            Voluptatum tenetur voluptate ipsum nesciunt, explicabo ducimus cumque eaque exercitationem dignissimos at temporibus accusantium saepe tempora molestiae, modi libero est iusto, atque impedit blanditiis architecto voluptatibus? Vitae ipsa consequuntur laudantium.
+            Facilis maiores expedita est ut dolor commodi suscipit tenetur reprehenderit possimus eveniet veritatis esse, laborum, hic dolorum voluptatem voluptates eius ratione! Incidunt obcaecati vitae quia ad voluptatem ipsum, commodi consequuntur.
+        </p>
     </div>
 </template>
 
