69018e2ec75f547162f87382c2b807270df7065d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-25 12:25:47 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- loginsignup/src/App.vue
+++ loginsignup/src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="" >
-    <AppHeader />
-  </div>
+    <HomePage />
+</div>
 </template>
 
 <script>
-import AppHeader from './components/AppHeader.vue'
+import HomePage from './components/HomePage.vue';
 
 export default {
   name: 'App',
   components: {
-    AppHeader
+    HomePage
   }
 }
 </script>
---
+++ loginsignup/src/components/HomePage.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="">
+        this is Homepage
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HomePage"
+    }
+</script>
