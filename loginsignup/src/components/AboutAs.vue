bc154d270d001d3196671cf03c8a976670ed1ee8
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-25 14:03:12 +0530

    Update AboutAs.vue
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- loginsignup/src/components/AboutAs.vue
+++ loginsignup/src/components/AboutAs.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="bg-gray-200 py-16 lg:px-20 md:px-12">
-        <h1 class="py-6 text-center lg:text-4xl md:text-2xl text-gray-500">About Us</h1>
+    <div class="bg-gray-200 py-16 lg:px-20 md:px-12 px-8 text-center">
+        <h1 class="py-6 text-center lg:text-4xl md:text-2xl text-xl text-gray-500">About Us</h1>
         <p class="">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum eius sed itaque autem facilis ipsam alias debitis doloremque unde vel. Quis animi officia sequi totam praesentium perferendis pariatur ipsum ducimus?
             Eum nam laborum, culpa tempora soluta, doloribus distinctio, sint nobis eaque excepturi minima incidunt alias? Facilis aut aliquid fuga quos aspernatur magnam unde distinctio possimus quaerat doloribus optio, consequuntur deleniti.
