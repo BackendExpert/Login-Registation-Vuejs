71d86d4ade20f4cfb7f3c76ce93db260d93a5186
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-25 13:14:47 +0530

    Update AppHeader.vue
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- loginsignup/src/components/AppHeader.vue
+++ loginsignup/src/components/AppHeader.vue
@@ -1,18 +1,34 @@
 <template>
-    <div class="w-full bg-none py-8 px-16 text-white lg:text-xl flex">
-      <BuildingOffice2Icon class="icon"/>  
-      <p class="pt-2 pl-2">JK Cars</p>
+    <div class="w-full bg-none py-8 md:px-16 px-4 text-white lg:text-xl flex justify-between">
+        <div class="flex">
+            <BuildingOffice2Icon class="icon"/>  
+            <p class="pt-2 pl-2">JK Cars</p>
+        </div>
+        <div class="">
+            <!-- button when mobile screen to open and close menu -->
+            <button @click="isNavOpen = !isNavOpen" class="sm:hidden">
+                <XMarkIcon class="{'hidden' : isNavOpen, 'block': !isOpen}" />
+                <ListBulletIcon class="{'hidden' : isNavOpen, 'block': !isOpen}" />
+            </button>
+        </div>
     </div>
 </template>
 
 <script>
-import { BuildingOffice2Icon } from '@heroicons/vue/24/solid';
+import { BuildingOffice2Icon, XMarkIcon, ListBulletIcon } from '@heroicons/vue/24/solid';
 
 export default {
     name: 'AppHeader',
     components: {
-        BuildingOffice2Icon
-    }
+        BuildingOffice2Icon,
+        XMarkIcon,
+        ListBulletIcon
+    },
+    data() {
+        return {
+            isNavOpen: false,
+        }
+    },
     
 }
 </script>
