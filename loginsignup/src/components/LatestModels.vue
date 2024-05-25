bf4c9621ed99c5b817337767b2dedf5e97fc54ea
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-25 14:01:04 +0530

    Update LatestModels.vue
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- loginsignup/src/components/LatestModels.vue
+++ loginsignup/src/components/LatestModels.vue
@@ -1,6 +1,36 @@
 <template>
     <div class="bg-white py-16 lg:px-20 md:px-12">        
         <h1 class="py-6 text-center lg:text-4xl md:text-2xl text-gray-500">Latest Models</h1>
+        <div class="md:grid grid-cols-4 gap-4 my-16">
+            <div class="bg-gray-200 py-6 px-4 rounded shadow-xl">
+                <h1 class="lg:text-3xl md:text-xl text-gray-500 font-semibold text-center">Car 1</h1>
+                <img src="https://wallpapercave.com/wp/wp2707503.jpg" alt="" class="rounded-md my-8">
+                <p class="">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem quae eum natus ipsa, repellendus dicta minus distinctio aliquam fuga dolor recusandae eius culpa consectetur dolores. Totam saepe magnam aliquid soluta?
+                </p>
+            </div>
+            <div class="bg-gray-200 py-6 px-4 rounded shadow-xl">
+                <h1 class="lg:text-3xl md:text-xl text-gray-500 font-semibold text-center">Car 1</h1>
+                <img src="https://wallpapercave.com/wp/wp2707503.jpg" alt="" class="rounded-md my-8">
+                <p class="">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem quae eum natus ipsa, repellendus dicta minus distinctio aliquam fuga dolor recusandae eius culpa consectetur dolores. Totam saepe magnam aliquid soluta?
+                </p>
+            </div>
+            <div class="bg-gray-200 py-6 px-4 rounded shadow-xl">
+                <h1 class="lg:text-3xl md:text-xl text-gray-500 font-semibold text-center">Car 1</h1>
+                <img src="https://wallpapercave.com/wp/wp2707503.jpg" alt="" class="rounded-md my-8">
+                <p class="">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem quae eum natus ipsa, repellendus dicta minus distinctio aliquam fuga dolor recusandae eius culpa consectetur dolores. Totam saepe magnam aliquid soluta?
+                </p>
+            </div>
+            <div class="bg-gray-200 py-6 px-4 rounded shadow-xl">
+                <h1 class="lg:text-3xl md:text-xl text-gray-500 font-semibold text-center">Car 1</h1>
+                <img src="https://wallpapercave.com/wp/wp2707503.jpg" alt="" class="rounded-md my-8">
+                <p class="">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem quae eum natus ipsa, repellendus dicta minus distinctio aliquam fuga dolor recusandae eius culpa consectetur dolores. Totam saepe magnam aliquid soluta?
+                </p>
+            </div>
+        </div>
     </div>
 </template>
 
