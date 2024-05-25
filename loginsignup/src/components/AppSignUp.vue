f18981246c496d753b2c3158957cdcb1fe77c311
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-25 14:38:47 +0530

    Update AppSignUp.vue
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- loginsignup/src/components/AppSignUp.vue
+++ loginsignup/src/components/AppSignUp.vue
@@ -1,6 +1,21 @@
 <template>
     <div class="">
-        Welcome to System
+        <div class="">
+            <form>
+                <div class="">
+                    <label for="" class="text-xl block my-2">Username : </label>
+                    <input type="text" name="" id="" class="h-12 2-full bg-white text-gray-700 rounded pl-2" required placeholder="Enter Username">
+                </div>
+                <div class="">
+                    <label for="" class="text-xl block my-2">Email : </label>
+                    <input type="email" name="" id="" class="h-12 2-full bg-white text-gray-700 rounded pl-2" required placeholder="Enter Email Address">
+                </div>
+                <div class="">
+                    <label for="" class="text-xl block my-2">Password : </label>
+                    <input type="password" name="" id="" class="h-12 2-full bg-white text-gray-700 rounded pl-2" required placeholder="Enter Password">
+                </div>
+            </form>
+        </div>
     </div>
 </template>
 
