0b894650f8e2b154324bf7cff06943f303db4fda
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-25 13:52:16 +0530

    Update AboutAs.vue
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- loginsignup/src/components/AboutAs.vue
+++ loginsignup/src/components/AboutAs.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="">
-        About Us
+    <div class="bg-gray-200 my-8">
+        <h1 class="py-6 text-center lg:text-4xl md:text-2xl">About Us</h1>
     </div>
 </template>
 
