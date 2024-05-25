4aa850d676bf1f3b871ff28b9de055a052358da6
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-25 13:54:58 +0530

    Create LatestModels.vue
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ loginsignup/src/components/LatestModels.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="bg-white py-16 lg:px-20 md:px-12">
+        Latest Models
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "LatestModels"
+    }
+</script>
