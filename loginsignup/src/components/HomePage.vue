42956e9231f7e95615cc0ca479b4e15184982027
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-25 12:26:42 +0530

    Update HomePage.vue
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- loginsignup/src/components/HomePage.vue
+++ loginsignup/src/components/HomePage.vue
@@ -1,11 +1,17 @@
 <template>
     <div class="">
+        asdasdasd
+        <AppHeader />
         this is Homepage
     </div>
 </template>
 
 <script>
+    import AppHeader from './AppHeader.vue';
     export default {
-        name: "HomePage"
+        name: "HomePage",
+        components: {
+            AppHeader
+        }
     }
 </script>
